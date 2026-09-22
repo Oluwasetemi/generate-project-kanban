8fc36595fab335915c2a4245c231f69cc777083e
oluwasetemi ojo <setemiojo@gmail.com>
2026-09-22 10:23:24 -0500

    chore: add lint and release scripts

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import setemiojo from '@setemiojo/eslint-config'
+
+export default setemiojo(
+  {
+    react: true,
+    typescript: {
+      tsconfigPath: './tsconfig.json',
+    },
+    ignores: ['dist/', 'node_modules/'],
+  },
+  {
+    rules: {
+      'no-undef': 'off',
+      'react-refresh/only-export-components': 'off',
+      'setemiojo/no-top-level-await': 'off',
+      'style/max-statements-per-line': 'off',
+      'ts/await-thenable': 'off',
+      'ts/no-unsafe-argument': 'off',
+      'ts/no-unsafe-assignment': 'off',
+      'ts/no-unsafe-return': 'off',
+      'ts/restrict-template-expressions': 'off',
+      'ts/strict-boolean-expressions': 'off',
+    },
+  },
+)
